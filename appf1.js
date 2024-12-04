0c0afdb4036ec310f40af94803bc623d1b77d5d6
Alexis Boshernitsan <alexis.boshernitsan@gmail.com>
2024-12-04 09:52:41 -0800

    working on it

---
+++ appf1.js
@@ -0,0 +1,38 @@
+const game = document.getElementById('game')
+const scoreDisplay = document.getElementById('score')
+
+
+const jeopardyCategories = {
+    {
+        genre: 'Driver Records'
+        question: []
+            {
+                question: 'Who holds the records for the most Formula 1 starts?'
+                answers: ['Lewis Hamilton', 'Fernando Alonso', 'Michael Schumaucher', 'Damon Hill']
+                correct: 'Fernando Alonso'
+                level: 'easy'
+            },
+            {
+                question: 'Who holds the records for the most Formula 1 wins?'
+                answers: ['Lewis Hamilton', 'Fernando Alonso', 'Michael Schumaucher', 'Damon Hill']
+                correct: 'Lewis Hamilton'
+                level: 'medium'
+            },
+            {
+                question: 'Max Verstappen and which other driver were the first ever to set identical qualifying lap times? (hint: it was this year)'
+                answers: ['Lando Norris', 'Lewis Hamilton', 'George Russell', 'Charles Leclerc']
+                correct: 'George Russell'
+                level: 'sort of difficult'
+            }
+            {
+                question: 'Who holds the records for the most Formula 1 wins?'
+                answers: ['Lewis Hamilton', 'Fernando Alonso', 'Michael Schumaucher', 'Damon Hill']
+                correct: 'Lewis Hamilton'
+                level: 'hard'
+            },
+    }
+    {
+        genre: 'WHERE'
+        question: []
+    }
+}

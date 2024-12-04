a2f7e3d3be95a87244aa62ea04bb7a3e7754bc20
Alexis Boshernitsan <alexis.boshernitsan@gmail.com>
2024-12-04 10:38:41 -0800

    midrange

--- appf1.js
+++ appf1.js
@@ -1,38 +1,38 @@
-const game = document.getElementById('game')
-const scoreDisplay = document.getElementById('score')
+const game = document.getElementById('game');
+const scoreDisplay = document.getElementById('score');
 
-
-const jeopardyCategories = {
+const jeopardyCategories = [
     {
-        genre: 'Driver Records'
-        question: []
+        genre: 'Driver Records',
+        questions: [
             {
-                question: 'Who holds the records for the most Formula 1 starts?'
-                answers: ['Lewis Hamilton', 'Fernando Alonso', 'Michael Schumaucher', 'Damon Hill']
-                correct: 'Fernando Alonso'
+                question: 'Who holds the record for the most Formula 1 starts?',
+                answers: ['Lewis Hamilton', 'Fernando Alonso', 'Michael Schumacher', 'Damon Hill'],
+                correct: 'Fernando Alonso',
                 level: 'easy'
             },
             {
-                question: 'Who holds the records for the most Formula 1 wins?'
-                answers: ['Lewis Hamilton', 'Fernando Alonso', 'Michael Schumaucher', 'Damon Hill']
-                correct: 'Lewis Hamilton'
+                question: 'Who holds the record for the most Formula 1 wins?',
+                answers: ['Lewis Hamilton', 'Fernando Alonso', 'Michael Schumacher', 'Damon Hill'],
+                correct: 'Lewis Hamilton',
                 level: 'medium'
             },
             {
-                question: 'Max Verstappen and which other driver were the first ever to set identical qualifying lap times? (hint: it was this year)'
-                answers: ['Lando Norris', 'Lewis Hamilton', 'George Russell', 'Charles Leclerc']
-                correct: 'George Russell'
+                question: 'Max Verstappen and which other driver were the first ever to set identical qualifying lap times? (hint: it was this year)',
+                answers: ['Lando Norris', 'Lewis Hamilton', 'George Russell', 'Charles Leclerc'],
+                correct: 'George Russell',
                 level: 'sort of difficult'
-            }
-            {
-                question: 'Who holds the records for the most Formula 1 wins?'
-                answers: ['Lewis Hamilton', 'Fernando Alonso', 'Michael Schumaucher', 'Damon Hill']
-                correct: 'Lewis Hamilton'
-                level: 'hard'
             },
-    }
+            {
+                question: 'Who holds the record for the most Formula 1 wins?',
+                answers: ['Lewis Hamilton', 'Fernando Alonso', 'Michael Schumacher', 'Damon Hill'],
+                correct: 'Lewis Hamilton',
+                level: 'hard'
+            }
+        ]
+    },
     {
-        genre: 'WHERE'
-        question: []
+        genre: 'WHERE',
+        questions: []
     }
-}
+];
